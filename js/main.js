1783ceb4ea9e35145cc3db9f5c650d34e0f87305
nurukan <chiakic0taan@gmail.com>
2025-04-21 01:02:34 +0900

    はじめて作成したおみくじゲームです！今日も楽しい日になりますように！

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
 'use strict';
 
 {
+    // const totalBtn = 6;
+    // let attemptCount = 0;
+    // const atari = Math.floor(Math.random()*totalBtn);
+
+    // const btnContainer = document.getElementById('btn-container');
+    
+
+    // for(let i = 0; i < totalBtn; i++){
+    //     const div = document.createElement('div');
+    //     div.classList.add('circle');
+
+    //     div.textContent = i;
+    //     div.style.color =  '#fcfcfc';
+    //     div.style.textShadow = `
+    //     -1px -1px 0 #a1bdbf,
+    //      1px -1px 0 #a1bdbf,
+    //     -1px  1px 0 #a1bdbf,
+    //      1px  1px 0 #a1bdbf`;
+    // }
+   
+    // function handleClick(count,button){
+    //     attemptCount++;
+
+    //     if(count === atari){
+    //         setTimeout(function(){
+    //             location.reload();
+    //         },1000);
+    //     }else{
+    //         div.disabled = true;
+    //         div.textContent = 'もう一回';
+    //         if(attemptCount === totalBtn){
+    //             setTimeout(function(){
+    //                 location.reload();
+    //             },1000);
+    //         }
+    //     }
+    // }
+
+    // }
+    const totalBtn = 6;
+    let attemptCount = 0;
+
     const atari = Math.floor(Math.random()*10);
 
-    for(let i = 1; i <= 6; i++){
+    for(let i = 1; i <= totalBtn; i++){
         const div = document.createElement('div');
-
         div.classList.add('circle');
 
         div.textContent = i;
@@ -15,8 +56,10 @@
          1px -1px 0 #a1bdbf,
         -1px  1px 0 #a1bdbf,
          1px  1px 0 #a1bdbf`;
+        div.style.cursor = 'pointer';
 
         div.addEventListener('click', ()=> {
+            attemptCount++;
 
             if(i === atari){
                 div.classList.add('atari');
@@ -31,7 +74,18 @@
             }else{
                 div.classList.add('hazure');
                 div.textContent = 'もう一回！';
+                if(attemptCount === totalBtn){
+                    const allBtns = document.querySelectorAll('.circle');
+                    allBtns.forEach(function(event){
+                        event.style.transition = 'opacity 2s';
+                        event.style.opacity = '0';
+                    });
+                    setTimeout(function(){
+                        location.reload();
+                    },2200);
+                   
             }
+        }
         });
         
         const backgroundSquare = document.querySelector('.background_square'); 
